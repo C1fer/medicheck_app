e0e3417ae6d300fdec740e204608c2838f7871d1
Rusbel <1103307@est.intec.edu.do>
2023-12-18 23:59:40 -0400

    Add global styles and initial onboarding

---
+++ lib/styles/app_styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static const kHeadingTextStyle = TextStyle(
+      fontSize: 22.0,
+      fontWeight: FontWeight.bold,
+      color: AppColors.deep_blue,
+  );
+
+  static const kMainTextStyle = TextStyle(
+      fontSize: 16.0,
+      color: AppColors.light_gray,
+  );
+
+
+  static const kSubTextStyle = TextStyle(
+      fontSize: 16.0,
+      color: AppColors.dark_gray,
+  );
+
+
+  static const kActionTextStyle = TextStyle(
+      fontSize: 15.0,
+      color: AppColors.jade_green,
+  );
+
+}
