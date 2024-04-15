d5ff46ffc7c44e2d5644f98163340983f7c85791
Rusbel <1103307@est.intec.edu.do>
2024-04-14 21:31:10 -0400

    Add replace underscores extension

--- lib/models/extensions/string_apis.dart
+++ lib/models/extensions/string_apis.dart
@@ -1,12 +1,17 @@
 extension StringExtension on String {
   String toSentenceCase() {
-    return this[0].toUpperCase() + this.substring(1).toLowerCase();
+    return trim()[0].toUpperCase() + substring(1).toLowerCase();
   }
 
   String toProperCase() {
-    return this
+    return trim()
         .split(" ")
         .map((e) => e[0].toUpperCase() + e.substring(1).toLowerCase())
         .join(' ');
   }
+
+  String replaceUnderScores(){
+    return replaceAll("_", " ");
+  }
+
 }
